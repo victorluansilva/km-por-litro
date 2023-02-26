0a700298775b6ec902cd84eac90b114541aba175
Victor Silva <victorluansilva@gmail.com>
2023-02-26 03:14:00 -0300

    Creating componente KmPerLiters

---
+++ src/components/KmPerLiters.js
@@ -0,0 +1,15 @@
+import './KmPerLiters.css';
+
+const KmPerLiters = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          KM por Litro
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default KmPerLiters;
